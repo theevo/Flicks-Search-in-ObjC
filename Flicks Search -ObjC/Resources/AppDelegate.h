0ea8d1f53cfd11ed3ff76738ca5076b9738f67fc
Theo Vora <theo.vora@gmail.com>
2020-03-27 09:22:34 -0600

    initial commit

---
+++ Flicks Search -ObjC/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flicks Search -ObjC
+//
+//  Created by theevo on 3/27/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
