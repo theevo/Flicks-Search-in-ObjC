4323a258e7511a11d6713b92bb7a62e754dbdfe2
Theo Vora <theo.vora@gmail.com>
2020-03-27 09:51:40 -0600

    add model

---
+++ Flicks Search -ObjC/Model/TTVMovie.h
@@ -0,0 +1,34 @@
+//
+//  TTVMovie.h
+//  Flicks Search -ObjC
+//
+//  Created by theevo on 3/27/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTVMovie : NSObject
+
+@property (nonatomic, readonly) NSString * title;
+@property (nonatomic, readonly) NSNumber * rating;
+@property (nonatomic, readonly) NSString * movieDescription;
+@property (nonatomic, readonly) NSString * posterPath;
+
+-(instancetype)initWithTitle:(NSString *)title
+                      rating:(NSNumber *)rating
+            movieDescription:(NSString *)movieDescription
+                  posterPath:(NSString *)posterPath;
+
+@end
+
+
+@interface TTVMovie (JSONConvertable)
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flicks Search -ObjC/Model/TTVMovie.m
@@ -0,0 +1,44 @@
+//
+//  TTVMovie.m
+//  Flicks Search -ObjC
+//
+//  Created by theevo on 3/27/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+#import "TTVMovie.h"
+
+@implementation TTVMovie
+
+-(instancetype)initWithTitle:(NSString *)title
+                      rating:(NSNumber *)rating
+            movieDescription:(NSString *)movieDescription
+                  posterPath:(NSString *)posterPath
+{
+    self = [super init];
+    if (self)
+    {
+        _title = title;
+        _rating = rating;
+        _movieDescription = movieDescription;
+        _posterPath = posterPath;
+    }
+    return self;
+}
+
+@end
+
+
+@implementation TTVMovie (JSONConvertable)
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSString *title = dictionary[@"title"];
+    NSNumber *rating = dictionary[@"vote_average"];
+    NSString *movieDescription = dictionary[@"overview"];
+    NSString *posterPath = dictionary[@"poster_path"];
+    
+    return [self initWithTitle:title rating:rating movieDescription:movieDescription posterPath:posterPath];
+}
+
+@end
